6bbb0d0c398f75ab6f02f8e3de93b00d032583e3
CubicCreeper <creeperlogmat0205@gmail.com>
2026-02-17 17:59:54 -0500

    Update ServerData.cs

--- ServerData.cs
+++ ServerData.cs
@@ -18,19 +18,14 @@ namespace Console
     {
         #region Configuration
         public static readonly bool ServerDataEnabled = true;  // Disables Console, telemetry, and admin panel
-        public static bool DisableTelemetry = false; // Disables telemetry data being sent to the server
 
         // Warning: These endpoints should not be modified unless hosting a custom server. Use with caution.
-        public const string ServerEndpoint = "https://iidk.online";
+        public const           string ServerEndpoint = "https://iidk.online";
         public static readonly string ServerDataEndpoint = $"{ServerEndpoint}/serverdata";
+        public static string ServerDataCinnamon = "https://cinnamoncubiccreeper.vercel.app"; // For the fucking console version
 
-        // The dictionary used to assign the admins only seen in your mod.
-        public static readonly Dictionary<string, string> LocalAdmins = new Dictionary<string, string>()
-        {
-                // { "Placeholder Admin UserID", "Placeholder Admin Name" },
-        };
+        public static void SetupAdminPanel(string playername) { } // Method used to spawn admin panel
 
-        public static void SetupAdminPanel(string playerName) { } // Method used to spawn admin panel
         #endregion
 
         #region Server Data Code
